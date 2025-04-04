95b69e68855c7456b0e79e690d342b5555d6ca47
Dan Pomohaci <dan.pomohaci@gmail.com>
2025-04-04 09:15:32 +0300

    update ent file to version 2.0

--- .ent.el
+++ .ent.el
@@ -5,27 +5,18 @@
 ;;; Code:
 
 ;; project settings
+
 (setq ent-project-home (file-name-directory (if load-file-name load-file-name buffer-file-name)))
 (setq ent-project-name "etrans")
-(setq ent-clean-regexp "~$\\|\\.tex$")
+(setq ent-clean-regexp "~$")
 (setq ent-project-orgfile "Notebook.org")
 
-(require 'ent)
+;; tasks
 
-(ent-tasks-init)
-
-(setq make (expand-file-name "makem.sh" ent-project-home))
-(setq init-opts " --install-deps --install-linters -v ")
-(setq opts " -s.sandbox  -e ent.el -v ")
-
-
-
-(task 'sandbox '() "create sandbox" '(lambda (&optional x) (concat make init-opts "-s.sandbox")))
-(task 'lint '() "lint the project" '(lambda (&optional x) (concat make opts "lint")))
-(task 'elsa '() "lint elsa the project" '(lambda (&optional x) (concat make opts "lint-elsa")))
-
-(task 'test '() "test the project" '(lambda (&optional x) (concat make opts "test")))
-
+(task :eldev-clean '(:clean) "clean tmp files" "eldev clean")
+(task :lint '(:eldev-clean) "emacs lisp lint" "eldev lint")
+(task :build '() "build package" "eldev build")
+(task :test '() "run tests" "eldev test")
 
 
 (provide '.ent)
